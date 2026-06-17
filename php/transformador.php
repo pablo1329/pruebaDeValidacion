0da20f2c680beab945a00df3ecc04930706ab430
pablo1329 <pablo_13_95@hotmail.com>
2026-06-17 10:45:17 -0300

    Corregir bugs en Transformador.php: inicializar variable, mejorar nombres, agregar validación y documentación PHPDoc

--- php/transformador.php
+++ php/transformador.php
@@ -1,30 +1,54 @@
 <?php 
 class Transformador {
 
+	/**
+	 * Convierte un array asociativo a un array indexado (0, 1, 2, ...)
+	 * @param array $datosSanitizados Array con datos a transformar
+	 * @return array Array con índices numéricos
+	 */
 	public static function devolverDatosParaConsultar(array $datosSanitizados): array {
+		// Inicializar array vacío
+		$datosParaConsultar = [];
 
-		foreach ($datosSanitizados as $key => $value) {
-			$datosDatosParaConsultar[] = $value;
+		// Recorrer y agregar valores
+		foreach ($datosSanitizados as $value) {
+			$datosParaConsultar[] = $value;
 		}
 
-		return $datosDatosParaConsultar;
+		return $datosParaConsultar;
 
 	}//fin function devolverDatosParaConsultar
 
-    public static function devolverArrayEstructurado(mysqli_result $resultado): array {
+	/**
+	 * Estructura los resultados de la BD en formato JSON amigable
+	 * Transforma resultados por filas en resultados por columnas
+	 * @param mysqli_result $resultado Resultado de la consulta
+	 * @return array Array con cantidad de resultados y datos estructurados
+	 * @throws Exception Si el resultado no es válido
+	 */
+	public static function devolverArrayEstructurado(mysqli_result $resultado): array {
 
-        $data = [];
+		// Validar que sea un resultado válido
+		if (!($resultado instanceof mysqli_result)) {
+			throw new Exception("El resultado proporcionado no es un objeto mysqli_result válido");
+		}
 
-        while ($row = $resultado->fetch_assoc()) {
-            foreach ($row as $key => $value) {
-                $data[$key][] = $value;
-            }
-        }
-        return [
-            'cantidadDeResultados' => count($data ? $data[array_key_first($data)] : []),
-            'datos' => $data
-        ];
-    }
+		$data = [];
+
+		// Recorrer resultados y estructurar por columnas
+		while ($row = $resultado->fetch_assoc()) {
+			foreach ($row as $key => $value) {
+				$data[$key][] = $value;
+			}
+		}
+
+		// Retornar cantidad de resultados y datos
+		return [
+			'cantidadDeResultados' => count($data ? $data[array_key_first($data)] : []),
+			'datos' => $data
+		];
+
+	}//fin function devolverArrayEstructurado
 
 }//fin class Transformador
 ?>
