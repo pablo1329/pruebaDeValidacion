a0cd08b8766e0c7104c3e6509d5eb74a5fa3cbc2
pablo1329 <pablo_13_95@hotmail.com>
2026-06-22 20:32:24 -0300

    Add files via upload

--- script/interaccion.js
+++ script/interaccion.js
@@ -1,9 +1,19 @@
-function detectarEnvioDeFormulario(){
+function obtenerIdsDeInputsDeFormularioPorSolicitud(){}//fin function obtenerIdsDeInputsDeFormularioPorSolicitud
+
+
+function obtenerDatosDeFormularioPorSolicitud(solicitud){
+
+	let inputFormulario = ['inputFecha', 'inputOrigenDeIngreso', 'inputImporte'];
+
+}//fin function obtenerDatosPorNombreDeBotonDeFormulario
+
+
+function detectarEnvioDeFormulario(solicitud){
 
 	let botonDeFormulario = document.getElementById('botonForm');
 
 	botonDeFormulario.addEventListener('click', ()=>{
-		
+		obtenerDatosDeFormularioPorSolicitud(solicitud);
 	});
 
 }//fin function detectarEnvioDeFormulario
@@ -15,7 +25,7 @@ function detectarInteraccionConBarraDeInicio(){
 
 	elementosDeListaPrincipal.forEach((element) => element.addEventListener('click', ()=>{
 		administrarVistaDeFormularioPorId(element.getAttribute('id'));
-		detectarEnvioDeFormulario();
+		detectarEnvioDeFormulario(element.getAttribute('id'));
 	}) );
 
 }//fin function detectarInteraccionConBarraDeInicio
