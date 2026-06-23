39e7bccee73fb6dd3d5a108edb6db2e6135d8f1b
pablo1329 <pablo_13_95@hotmail.com>
2026-06-23 08:36:36 -0300

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,1 +1,22 @@
+<?php
+spl_autoload_register(function ($nombre_clase) {
+    // Busca el archivo basado en el nombre de la clase
+    $archivo = __DIR__ . '/' . $nombre_clase . '.php';
+    if (file_exists($archivo)) {
+        require_once $archivo;
+    }
+});
 
+$config = require __DIR__ . '/config.php';
+
+$MysqliDB = new MysqliDB($config['host'], $config['user'], $config['pass'], $config['db']);
+
+$CRUD = new CRUD();
+
+$Validar = new Validar();
+
+$api = new ApiControlador($Validar, $MysqliDB, $CRUD);
+
+$api->gestionarRespuesta();
+
+?>
